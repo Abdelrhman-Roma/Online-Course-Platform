3a5b62e58c984ef765cd91d47b8f2f26a1d58458
Abdelrhman <abdelrhman.roma2006@gmail.com>
2026-04-25 15:02:46 +0300

    mobile icon and hero section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+
+
+    if (toggle) {
+        toggle.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
+        });
+    }
+});
